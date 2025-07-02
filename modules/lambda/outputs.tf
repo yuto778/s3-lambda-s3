35f58b72de8e3240f7c910a1bd93e48c078ec775
yuto778 <yuto4949@i.softbank.jp>
2025-07-03 08:06:52 +0900

    output

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -2,3 +2,7 @@ output "function_arn" {
   value = aws_lambda_function.this.arn
 
 }
+
+output "function_name" {
+  value = aws_lambda_function.this.function_name
+}
--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,11 @@ output "s3_source_bucket_id" {
 output "s3_target_bucket_id" {
   value = module.target_s3.bucket_id
 }
+
+output "event_queue_url" {
+  value = module.event_queue.queue_url
+}
+
+output "lambda_function_name" {
+  value = module.processor_lambda.function_name
+}
